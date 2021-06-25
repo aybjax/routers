f03a2b2894760ba947e7909ccc093a98571b544b
aybjax <aybat93@gmail.com>
2021-06-26 03:49:28 +0600

    gorillaMux_query

---
+++ gorillaMux_query.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func QueryHandler(w http.ResponseWriter, r *http.Request) {
+	queries := r.URL.Query()
+	
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, "Category is: %s\n", queries["category"])
+	fmt.Fprintf(w, "ID is: %s\n", queries["id"])
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/articles", QueryHandler)
+	r.Queries("id", "category")
+
+	srv := &http.Server {
+		Handler: r,
+		Addr: "127.0.0.1:8080",
+		WriteTimeout: 15*time.Second,
+		ReadTimeout: 15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
